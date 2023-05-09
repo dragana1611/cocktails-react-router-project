f9a1433de24afaa1f927ed2745e85b3bb0c89cbd
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-09 22:17:02 +0200

    initial commit

---
+++ src/components/Cocktail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cocktail = ({ id, name, image, info, glass }) => {
+  return (
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+      </div>
+    </article>
+  );
+};
+
+export default Cocktail;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        velit non quo officiis dicta doloribus quidem in eos architecto? Maiores
+        neque non alias impedit adipisci mollitia reprehenderit corporis
+        similique ea assumenda aperiam ipsum ab sunt, aliquam harum fuga
+        voluptate veritatis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
